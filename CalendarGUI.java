2c162f044ee0af208ac40f9af13da8fd27b815c5
Nathanael William O'Donnell <nodonnel@gmail.com>
2019-03-20 13:38:43 -0400

    Removed some clutter in the code for Calendar.java and CalendarGUI.java

--- CalendarGUI.java
+++ CalendarGUI.java
@@ -2,12 +2,6 @@
 import java.awt.CardLayout;
 import java.util.Arrays;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Group 2
