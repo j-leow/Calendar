61ab766058accb0c7ffb25065509e6531dec9426
Nathanael William O'Donnell <nodonnel@gmail.com>
2019-03-20 13:29:01 -0400

    now Calendar.generate() works with leap years

--- calendar.java
+++ calendar.java
@@ -17,19 +17,26 @@ public class Calendar {
     public static String[] generate(int month, int year) {
         String[] calendarArray = new String[42];
         int startDay = startDay(month, year);
+        int numDays;
+        
+        if (month == 2 && isLeap(year)) {
+            numDays = 29;
+        } else {
+            numDays = days[month];
+        }
 
         for (int i = 0; i < startDay; i++) {
             calendarArray[i] = " ";
         }
-        
-        for (int i = 0; i <= days[month]; i++) {
+
+        for (int i = 0; i <= numDays; i++) {
             calendarArray[i + startDay] = Integer.toString(i + 1);
         }
-        
-        for (int i = startDay + days[month]; i < calendarArray.length; i++) {
+
+        for (int i = startDay + numDays; i < calendarArray.length; i++) {
             calendarArray[i] = " ";
         }
-        
+
         return calendarArray;
     }
 
@@ -39,5 +46,25 @@ public class Calendar {
         int m = month + 12 * ((14 - month) / 12) - 2;
         int d = (1 + x + (31 * m) / 12) % 7;
         return d;
+    }
+
+    private static boolean isLeap(int year) {
+        boolean flag = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    flag = true; // if divisible by 400, then is a leap year
+                } else {
+                    flag = false; // if divisible by 4 and 100, but not 400, then not a leap year
+                }
+            } else {
+                flag = true; // if divisble by 4 but not 100, then is a leap year
+            }
+        } else {
+            flag = false;
+        }
+
+        return flag;
     }
 }
